79334babd3e4ef20c24cd8cd8e5720aba0570dda
Remeran <alexisduprey@gmail.com>
2023-09-21 10:32:08 -0400

    title screen
    - added text title
    - added start and exit buttons
    - created UI manager script to handle scene flow

---
+++ Programming Theory Project/Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class UIManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
